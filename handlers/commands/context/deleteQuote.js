0a4d678d65fe314f0cec8c919e850c297dc272c9
kennyhazzar <56494597+kennyhazzar@users.noreply.github.com>
2021-09-26 17:41:12 +0300

    Удаление цитаты

---
+++ handlers/commands/context/deleteQuote.js
@@ -0,0 +1,25 @@
+﻿const Query = require('../../../stores/context/quote.js')
+
+const deleteQueryHandler = async ctx => {
+    if (ctx.chat.type !== 'private') return null
+
+    var userMessage = ctx.message.text
+
+    const query = await Query.getQuote()
+
+    const commandInstance = userMessage.substring(userMessage.search(' '), userMessage.length).trim()
+
+    if (commandInstance === '/deleteQuote') {
+        ctx.replyWithHTML(`Список шаблонов приветственного сообщения:\n(Текст, айди)\n
+        ${query.length == 0 ? "Пусто\n" : query.map((item, index) => {
+            return "\n" + `${index + 1}. ${item.data}\n<strong>${item._id}</strong>\n`
+        }).join('')}\n\nДля удаления шаблона, введи /deleteQuote &lt;айди из списка&gt;`)
+    } else {
+        const quoteDelete = await Query.getQuoteOne({_id: commandInstance})
+        await Query.removeQuote(commandInstance)
+        ctx.replyWithHTML(`<strong>Цитата с содержанием</strong>:\n${quoteDelete.data}\n<strong>удалено</strong>`)
+        
+    }
+}
+
+module.exports = deleteQueryHandler
