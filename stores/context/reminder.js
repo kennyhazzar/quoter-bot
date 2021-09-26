4c779d85ea6190a4a0cf388f980f20976ddaac56
kennyhazzar <56494597+kennyhazzar@users.noreply.github.com>
2021-09-26 17:40:00 +0300

    Прослойка для сохранения данных в бд

---
+++ stores/context/reminder.js
@@ -0,0 +1,50 @@
+﻿const Datastore = require('nedb')
+const nedbPromise = require('nedb-promise')
+
+const db = new Datastore({ autoload: true, filename: 'data/context/Reminder.db' })
+const Reminder = nedbPromise.fromInstance(db)
+
+const compactDb = () => db.persistence.compactDatafile()
+
+const isReminderValid = (reminderNumber) => typeof reminderNumber == 'number' ? true : false
+
+const getReminderOne = (query = {}) => Reminder.findOne(query)
+
+const getReminder = (query = {}) => Reminder.find(query)
+
+const getCurrentReminder = () => Reminder.findOne({ isSelect: true })
+
+const addReminder = (reminderNumber) =>
+    isReminderValid(reminderNumber) ?
+        Reminder.insert({ data: reminderNumber }) : null
+
+
+const updateCurrentReminder = (newReminder) => {
+    isReminderValid(newReminder)
+        ? Reminder.update({ isSelect: true }, { $set: { data: newReminder } }) : null
+    compactDb()
+}
+
+const changeCurrentReminder = idNewCurrent => {
+    Reminder.update(
+        { isSelect: true },
+        { $set: { isSelect: false } }
+    )
+    Reminder.update({ _id: idNewCurrent }, { $set: { isSelect: true } })
+    compactDb()
+}
+
+const removeReminder = (_id) => {
+    Reminder.remove({ _id })
+    compactDb()
+}
+
+module.exports = {
+    getCurrentReminder,
+    getReminder,
+    getReminderOne,
+    addReminder,
+    updateCurrentReminder,
+    changeCurrentReminder,
+    removeReminder
+}
