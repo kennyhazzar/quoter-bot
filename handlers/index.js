9e6d67afaeaec201b01bf18c683025d04f67e0b9
kennyhazzar <56494597+kennyhazzar@users.noreply.github.com>
2021-09-26 17:40:10 +0300

    Костыль в коде образовался..

--- handlers/index.js
+++ handlers/index.js
@@ -1,6 +1,12 @@
 ﻿const { Composer } = require('telegraf')
-
 const handler = new Composer()
+
+handler.use((require('./actions/addQuotes.js')).middleware())
+
+handler.action("AddQuotes", ctx => {
+    console.log('AddQuotes')
+    ctx.scene.enter("settingAddQuotesScene")
+})
 
 handler.use(
     require('./commands'),
