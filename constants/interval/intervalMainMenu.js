09ed1ffe29d82000385873be50892e0a8d2dcb77
kennyhazzar <56494597+kennyhazzar@users.noreply.github.com>
2021-10-17 08:08:24 +0300

    add constant objects

---
+++ constants/interval/intervalMainMenu.js
@@ -0,0 +1,11 @@
+﻿const intervalMainMenu = {
+    messageText: "Для добавления нового интервала, введи /interval <Шаблон интервала> <Категория>\nЕсли категория не выбрана, то цитаты будут браться все подряд",
+    inline_keyboard: {
+        reply_markup: {
+            inline_keyboard: [[{ "text": "Шаблоны", "callback_data": "IntervalTemplates" }]],
+            force_reply: true
+        }
+    }
+}
+
+module.exports = {intervalMainMenu}
---
+++ constants/interval/intervalTemplates.js
@@ -0,0 +1,13 @@
+﻿const intervalTemplates = {
+    helpMessage: "Используй во втором аргументе команды /interval следующие шаблоны\n",
+    templateKey: [
+        { title: "*", description: "Один раз в сутки", timeInterval: 86400000 },
+        { title: "**", description: "Два раза в сутки", timeInterval: 43200000 },
+        { title: "***", description: "Три раза в сутки", timeInterval: 28800000 },
+        { title: "*1", description: "Каждый час", timeInterval: 3600000 },
+        { title: "*2", description: "Каждые два часа", timeInterval: 7200000 },
+        { title: "*3", description: "Каждые три часа", timeInterval: 10800000 },
+    ]
+}
+
+module.exports = { intervalTemplates }
